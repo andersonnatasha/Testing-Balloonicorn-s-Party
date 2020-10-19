908a800a93992570636a996434eaf7f601cf7400
Natasha Anderson <andersonnatashaa@gmail.com>
2020-10-19 19:46:01 +0000

    added tests for is_mel and most_and_least_common_type

--- party.py
+++ party.py
@@ -8,15 +8,38 @@ app.secret_key = "SECRETSECRETSECRET"
 
 
 def is_mel(name, email):
-    """Is this user Mel?"""
+    """Is this user Mel?
 
-    return name == "Mel Melitpolski" or email == "mel@ubermelon.com"
+        >>> is_mel('Mel Melitpolski', 'mel@ubermelon.com')
+        True
+
+        >>> is_mel('Judith Butler', 'judith@awesome.com')
+        False
+
+        >>> is_mel('Mel Melitpolski', 'judith@awesome.com')
+        True
+
+        >>> is_mel('Judith Butler', 'mel@ubermelon.com')
+        True
+
+    """
+
+    return name == 'Mel Melitpolski' or email == 'mel@ubermelon.com'
 
 
 def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
 
     Return most and least common treat types in tuple of format (most, least).
+
+        >>> most_and_least_common_type([
+        ...     {"type": "dessert"},
+        ...     {"type": "dessert"},
+        ...     {"type": "app"},
+        ...     {"type": "dessert"},
+        ...     {"type": "app"},
+        ...     {"type": "drink"}])
+        ('dessert', 'drink')
     """
 
     types = {}
